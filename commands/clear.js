9043ee952bee4904ed7082e94e10cd22c997a06d
Timofey Lichtenberg <45564102+Noxious-Regression@users.noreply.github.com>
2019-01-25 17:38:10 +0300

    Фикс clear.js

---
+++ commands/clear.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+const colours = require("../colours.json");
+const prefix = botconfig.prefix
+
+module.exports.run = async (bot, message, args) => {
+
+    //!clear 15
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(" У вас недостаточно прав :x:");
+    if(!args[0]) return message.channel.send(":warning: Вы забыли указать число");
+    message.channel.bulkDelete(args[0]).then(() => {
+      message.channel.send(`✅ Удалил ${args[0]} сообщений.`);
+    });
+}
+
+// module.exports.name = {
+//   name: "clear"
+// }
+
+
+module.exports.config = {
+  name: "clear",
+  aliases: [],
+  description: "clear"
+  }
