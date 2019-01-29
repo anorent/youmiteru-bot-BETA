c7657a6c473a169d3e4d334e921a1582e67c1734
Timofey Lichtenberg <45564102+Noxious-Regression@users.noreply.github.com>
2019-01-29 22:33:34 +0300

    Переработан play.js
    !!! НА ДАННЫЙ МОМЕНТ ДОБАВИТЬ МОЖНО ТОЛЬКО ОДИН ТРЕК !!!
    +
    Не исключаю, что могу полностью переработать команду (в частности для создания очереди на треки)

--- commands/play.js
+++ commands/play.js
@@ -1,34 +1,19 @@
 module.exports.run = async (bot, message, args) => {
-  function play(connection, message) {
-      var server = servers[message.guild.id];
 
-      server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
-
-      server.queue.shift();
-
-      server.dispatcher.on("end", function() {
-        if(server.queue[0]) play(connection, message);
-          else connection.disconnect();
-      });
-  }
-
-  var servers = {}
-
-  if(!args[1]) return message.channel.send('**Укажите ссылку на трек**');
+  if(!args[0]) return message.channel.send('**Укажите ссылку на трек**');
   if(!message.member.voiceChannel) return message.channel.send('**Вы должны быть в голосовом чате**');
 
-  if(!servers[message.guild.id]) server[message.guild.id] = {
-    queue: []
-  }
+  let validate = await ytdl.validateURL(args[0]);
 
-  var server = servers[message.guild.id];
+  if(!validate) return message.channel.send('Введите **действительную** ссылку');
 
-  server.queue.push(args[1]);
+  let info = await ytdl.getInfo(args[0]);
 
-  if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-    play(connection, message);
-  });
+  let connection = await message.member.voiceChannel.join();
 
+  let dispatcher = await connection.playStream(ytdl(args[0], {filter: 'audioonly'}));
+
+  message.channel.send(`Сейчас играет **${info.title}**`);
 }
 
 module.exports.config = {
