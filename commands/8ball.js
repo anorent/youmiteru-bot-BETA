a0ec53f3988253c1bfa547a9be79f5aa8b6bcc33
R-Yuliya <46893382+R-Yuliya@users.noreply.github.com>
2019-02-08 22:03:02 +1000

    Add files via upload

---
+++ commands/8ball.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    if (!args[1]) return message.reply("М,а где вопрос?");
+    let replies = ["Да", "Нет", "Ну,не знаю", "Сейчас не могу дать точного ответа"];
+
+    let result = Math.floor((Math.random() * replies.length));
+    let question = args.slice(0).join(" ");
+
+    let ballEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag)
+        .setColor("#FF9900")
+        .addField("Вопрос:", question)
+        .addField("Ответ:", replies[result]);
+
+    message.channel.send(ballEmbed);
+}
+
+module.exports.config = {
+    name: "8ball",
+    aliases: ["8b"],
+    description: "Магический шарик"
+}
