610ef150490b7e87e9daec1626459275e40f1c27
R-Yuliya <46893382+R-Yuliya@users.noreply.github.com>
2019-02-08 22:13:34 +1000

    NEKO HENTAI

---
+++ commands/nhent.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+const colours = require("../colours.json");
+const superagent = require("superagent")
+
+
+module.exports.run = async(bot, message, args) => {
+    let msg = await message.channel.send("Загрузка...")
+
+    if (!message.channel.nsfw) return message.channel.send("Тут низя такое смотреть! Перейдите в NSFW канал");
+
+    let { body } = await superagent
+        .get('https://nekos.life/api/v2/img/nsfw_neko_gif')
+        //console.Log(body.file)
+    if (!{ body }) return message.channel.send("Ой,тут какая-то ошибка.Может попробуешь ещё раз?^^")
+
+    let nhEmbed = new Discord.RichEmbed()
+        .setColor(colours.violet)
+        .setAuthor("Hentaaai ♥", message.guild.iconURL)
+        .setImage(body.url)
+        .setTimestamp()
+        .setFooter(`Витуся^^ | Зефирка♥#6382`, bot.user.displayAvatarURL)
+
+    message.channel.send({ embed: nhEmbed })
+
+    msg.delete();
+}
+
+
+module.exports.config = {
+    name: "nhent",
+    aliases: ["nh"],
+    description: "Hentaaaaaaai"
+}
