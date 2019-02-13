86e04f77bc807964bc5769192f15c3d847b62621
R-Yuliya <46893382+R-Yuliya@users.noreply.github.com>
2019-02-13 21:30:52 +1000

    Add files via upload

---
+++ commands/say.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+const colours = require("../colours.json");
+const superagent = require("superagent")
+
+module.exports.run = async(bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Искала,искала и не нашла у вас нужных прав")
+    let botmessage = args.join(" ");
+    message.delete().catch();
+    message.channel.send(botmessage);
+}
+
+module.exports.config = {
+    name: "say",
+    aliases: [],
+    description: "Повторение сообщения"
+}
