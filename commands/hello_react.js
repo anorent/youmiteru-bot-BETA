99d631c11f3ed105a055d6665bea8077631b99d6
Timofey Lichtenberg <45564102+Vism-noise@users.noreply.github.com>
2019-03-02 02:45:34 +0300

    Delete hello_react.js

--- commands/hello_react.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-  if(message.content === 'Привет', 'Здравствуйте', 'Даров', 'Здарова') {
-    message.react('👋');
-  }
-}
-
-module.exports.config = {
-  description: "Реагирует на сообщения приветствия"
-}
