4c2b869c71ab5d68f613d9aa49cda8bca6bc5b71
R-Yuliya <46893382+R-Yuliya@users.noreply.github.com>
2019-03-07 20:07:46 +1000

    install package "randompuppy"

---
+++ commands/porn.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy')
+
+module.exports.run = async(bot, message, args) => {
+  if(!message.channel.nsfw) {return message.channel.send(`Тут низя такое смотреть! Перейдите в NSFW канал `)}
+  else{
+  randomPuppy('porn')
+            .then(url => {
+                const embed = new Discord.RichEmbed()
+                
+                .setTitle(`Порнушка`)
+                .setTimestamp()
+                .setFooter(`For ${message.author.tag}`)
+                .setImage(url)
+                .setColor('RANDOM')
+    return message.channel.send({ embed });
+            })
+  }
+}
+module.exports.config = {
+	name: "porn",
+  aliases: [],
+  description: "porn"
+}
