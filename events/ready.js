8d8e595794ba8e2e77f09a28bf367e2c5074a644
anorent <46894885+anorent@users.noreply.github.com>
2019-03-31 20:39:46 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,12 @@
-﻿const Discord = require("discord.js")
+const Discord = require("discord.js")
 
 
 module.exports = bot => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity(`Смотрит на  ${bot.users.size} пользователей`, { type: "STREAMING", url: "https://www.twitch.tv/strandable" });
+    bot.user.setActivity(`Prefix: ! | See ${bot.users.size} users`, { type: "STREAMING", url: "https://www.twitch.tv/strandable" });
 
     let statuses = [
-        `*Youmiteru one love*` 
-        `Prefix: !`
+        `*Thinking about her coder*` 
     ]
 
     setInterval(function() {
@@ -17,3 +16,4 @@ module.exports = bot => {
     }, 1800000)
 
 }
+
