430b8b8ad45e9dd9c14769f93237994c43ec108b
anorent <46894885+anorent@users.noreply.github.com>
2019-04-14 09:56:45 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -19,9 +19,13 @@ let voice = 0;
                 bot.guilds.get("485018137134235648").channels.filter(chan => chan.type === 'voice').forEach((channel) => {voice += channel.members.size});
 bot.channels.get("566351514831028234").setName(`Голосовой\u2009онлайн:\u2009${voice}`)
 }, 5000)
-client.on('message', message => {
-    const channel = message.guild.channels.find(id=566878376980381706);
-    channel.setName(`${message.guild.users.size} пользователей`);
-
+bot.on('message', message => {
+    try{    
+        const channel = message.guild.channels.find(id=айди_канала);
+            channel.setName(`${message.guild.users.size} пользователей`);
+    catch(err){
+        // nothing interesting
+    }
+});
 }
 
