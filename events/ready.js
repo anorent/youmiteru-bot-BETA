dbe7798fe662aa6ebea6efb0e990df17694096e1
anorent <46894885+anorent@users.noreply.github.com>
2019-05-11 09:04:28 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -14,29 +14,4 @@ module.exports = bot => {
         bot.user.setActivity(status, { type: "STREAMING" })
 
         }, 1800000)
-
-   bot.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.find(ch => ch.name === '🙋┋join-log');
-    if (!channel) return;
-    const embed = new Discord.RichEmbed()
-        .setTitle("Добро Пожаловать!")
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setColor(0x8a57e5)
-        .setDescription("Здравствуй " + member.user.username + ".\n Добро Пожаловать на Youmiteru!")
-        .setImage(member.user.avatarURL)
-        .setTimestamp()
-    channel.send({embed});
-});
-bot.on("guildMemberRemove", (member) => {
-    const channel = member.guild.channels.find(ch => ch.name === '🏃┋leave-log');
-    if (!channel) return;
-    const embed = new Discord.RichEmbed()
-        .setTitle("User Left")
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setColor(0x8a57e5)
-        .setDescription("Пользователь " + member.user.username + " ливнул .")
-        .setImage(member.user.avatarURL)
-        .setTimestamp()
-    channel.send({embed});
-});
 }
